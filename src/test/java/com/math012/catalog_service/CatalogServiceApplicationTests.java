750d65a529915fb0bda4e3dff46f13fad4785ad0
Math012 <math111.0811@gmail.com>
2025-08-12 20:22:28 -0300

    Primeiro commit

---
+++ src/test/java/com/math012/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
